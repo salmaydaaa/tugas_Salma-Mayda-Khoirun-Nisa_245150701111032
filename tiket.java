6922ecb1ce8061f264da5cbe0320947487da8757
salmaydaaa <salmayda265@gmail.com>
2025-02-24 22:49:34 +0700

    tiket.java

---
+++ tiket.java
@@ -0,0 +1,30 @@
+package Praktikum;
+
+public class Tiket {
+    private String nomorTiket;
+    private Film film;
+    private Studio studio;
+    private int nomorKursi;
+    private String namaPenonton;
+
+    // Constructor
+    public Tiket(String nomorTiket, Film film, Studio studio, int nomorKursi, String namaPenonton) {
+        this.nomorTiket = nomorTiket;
+        this.film = film;
+        this.studio = studio;
+        this.nomorKursi = nomorKursi;
+        this.namaPenonton = namaPenonton;
+    }
+
+    // Menampilkan detail tiket
+    public void tampilkanTiket() {
+        System.out.println("\n=== Detail Tiket ===");
+        System.out.println("Nomor Tiket   : " + nomorTiket);
+        System.out.println("Nama Penonton : " + namaPenonton);
+        System.out.println("Film          : " + film.getJudul());
+        System.out.println("Studio        : " + studio.getNomorStudio());
+        System.out.println("Nomor Kursi   : " + nomorKursi);
+        System.out.println("Harga Tiket   : " + film.getHargaTiket());
+        System.out.println("=======================");
+    }
+}
